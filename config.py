7e37b1f72b7693f81a0babe80f744322f24644a6
JasonMartino <jason.martino@chevron.com>
2024-10-05 11:25:29 +0800

    Update config.py

--- config.py
+++ config.py
@@ -19,18 +19,18 @@ class Config(object):
 
     ### Info for MS Authentication ###
     ### As adapted from: https://github.com/Azure-Samples/ms-identity-python-webapp ###
-    CLIENT_SECRET = "ENTER_CLIENT_SECRET_HERE"
+    #CLIENT_SECRET = "ENTER_CLIENT_SECRET_HERE"
     # In your production app, Microsoft recommends you to use other ways to store your secret,
     # such as KeyVault, or environment variable as described in Flask's documentation here:
     # https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
-    # CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+    CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
     # if not CLIENT_SECRET:
     #     raise ValueError("Need to define CLIENT_SECRET environment variable")
 
     AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app, else put tenant name
     # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
 
-    CLIENT_ID = "ENTER_CLIENT_ID_HERE"
+    CLIENT_ID = os.environ.get('CLIENT_ID')
 
     REDIRECT_PATH = "/getAToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
 
@@ -39,3 +39,6 @@ class Config(object):
     SCOPE = ["User.Read"] # Only need to read user profile for this app
 
     SESSION_TYPE = "filesystem"  # Token cache will be stored in server-side session
+
+    REDIRECT_URI = "https://udacitycms-hsggc0fbb9cpfyav.canadacentral-01.azurewebsites.net/getAToken"
+    LOGOUT_URI = "https://udacitycms-hsggc0fbb9cpfyav.canadacentral-01.azurewebsites.net/login"
